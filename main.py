d708817d050c282d56750bf911166088e732d617
Tami <tamiortami@gmail.com>
2025-05-23 10:51:47 +0000

    calculator

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 # user_name = parameter
-def say_hello(user_name):
+# "=" is default parameter
+def say_hello(user_name="anonymous", user_age=0):
   print("hello", user_name, "how are you?")
+  print(user_name, "is", user_age, "years old.")
 
 
 # tami = argument
-say_hello("tami")
+say_hello("tami", 20)
+say_hello()
+
+
+# calculator
+def plus(a=0, b=0):
+  print(a + b)
+
+def minus(a=0, b=0):
+  print(a - b)
+
+def multiply(a=0, b=0):
+  print(a * b)
+
+def divide(a=0, b=1):
+  print(a / b)
+
+def power(a=0, b=1):
+  print(a ** b)
+
+plus(3, 4)
+minus(5, 3)
+multiply(2, 8)
+divide(6, 3)
+power(3, 3)
+
+plus()
+minus()
+multiply()
+divide()
+power()
